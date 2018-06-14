881fb3f803493063ef625d3314a24120a6ebd4b9
Nate Miller <natekmiller96@gmail.com>
2018-06-14 17:25:30 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,9 +24,9 @@ def won?(board)
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
-    puts "Oh hi, Mark"
+
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_array
+      return [position_1, position_2, position_3]
     end
   end
 end
